e3b63c546c416350dc830983bf60566dba083c22
manavdewan02 <72863213+manavdewan02@users.noreply.github.com>
2022-05-28 16:25:30 +0530

    Update use ponters to sort string in alphabatical order.cpp

--- use ponters to sort string in alphabatical order.cpp
+++ use ponters to sort string in alphabatical order.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    string str="india";
-    int n=str.length();
+    char str[20]="india";
+    char *s=str;
+    int n=0;
+    for(int i=0;s[i]!='\0';i++){
+        n++;
+    }
     for(int i=0;i<n;i++){
         for(int j=0;j<n-1-i;j++){
-            if(str[j]-97>str[j+1]-97){
-                swap(str[j],str[j+1]);
+            if(s[j]-97>s[j+1]-97){
+                swap(s[j],s[j+1]);
             }
         }
     }
